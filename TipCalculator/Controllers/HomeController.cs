294e5f454140b7169214da4a9832e4d261ab8935
ELA620 <71036591+ELA620@users.noreply.github.com>
2020-12-12 11:00:46 -0800

    Add files via upload

---
+++ TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿namespace TipCalculator.Controllers
+
+{    
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+    
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+                return View();
+        }
+
+
+        [HttpPost]
+       
+            public IActionResult Index(Calculator calculator)
+            {
+            if(ModelState.IsValid)
+
+            {
+                ViewBag.Fifteen = calculator.CalculateTip(.15);
+                ViewBag.Twenty = calculator.CalculateTip(.20);
+                ViewBag.TwentyFive = calculator.CalculateTip(.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+                return View(calculator);
+            }
+        }
+}
